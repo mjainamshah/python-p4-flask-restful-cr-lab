74e18ec6c1c783d910ca2de250426e03d9160010
mjainamshah <jainam.shah@student.moringaschool.com>
2024-01-10 13:22:06 +0300

    COMPLETED

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -17,11 +16,66 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
+
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.get(id)
+
+        if plant:
+            if 'name' in data:
+                plant.name = data['name']
+            if 'image' in data:
+                plant.image = data['image']
+            if 'price' in data:
+                plant.price = data['price']
+
+            db.session.commit()
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response({'message': 'Plant deleted successfully'}, 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to Plant API"})
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
